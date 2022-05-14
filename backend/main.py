d3665a206f0d4bcdbc26cf9b3dda7f7fae1cfa2d
Sergio Morales <sergiomorales@Sergios-MacBook-Pro-2.local>
2022-05-14 08:54:08 +0200

    changed folder name and add to api

--- backend/main.py
+++ backend/main.py
@@ -5,24 +5,21 @@ from fastapi import File
 from fastapi import UploadFile
 from io import StringIO
 from pydantic import BaseModel
-
-
+from winequality.inference import make_predictions
 
 app = FastAPI()
 
 
 class Response(BaseModel):
-	predictions: list[int]
+    predictions: list[int]
 
-	# class Config:
-	# 	arbitrary_types_allowed = True
+
+# class Config:
+# 	arbitrary_types_allowed = True
 
 
 @app.post("/predict", response_model=Response)
 async def predict(file: UploadFile = File(...)):
-	data = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), header=None, encoding='utf-8')
-	data = np.array(data)
-
-	return Response(predictions=[1,2,3,4])
-
-
+    data = pd.read_csv(StringIO(str(file.file.read(), 'utf-8')), header=None, encoding='utf-8')
+    # data = np.array(data)
+    return Response(make_predictions(data))
