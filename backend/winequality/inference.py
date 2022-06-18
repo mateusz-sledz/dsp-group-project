757bbc21501855d07dbb23575ebc8ed552e16e7e
MeghaRam-ai <meghasudheendran92@gmail.com>
2022-06-18 09:00:28 +0200

    airflow updates

--- backend/winequality/inference.py
+++ backend/winequality/inference.py
@@ -1,9 +1,10 @@
+
 import pandas as pd
 import joblib
 
 
 def make_predictions(data: pd.DataFrame) -> dict[str, str]:
     df_copy = data.copy()
-    model = joblib.load('models/RANDOM_FOREST_MODEL')
-
+    model = joblib.load('../models/RANDOM_FOREST_MODEL')
     return model.predict(df_copy)
+
--- backend/winequality/pipeline.py
+++ backend/winequality/pipeline.py
@@ -4,11 +4,11 @@ import joblib
 
 
 def load_pipeline_data() -> pd.DataFrame:
-    data = pd.read_csv('../backend/data/injection_data1.csv')
+    data = pd.read_csv('../data/winequality-red.csv')
     return data
 
 
 def run_model_background(data: pd.DataFrame) -> dict[str, str]:
     df_copy = data.copy()
-    model = joblib.load('../backend/models/RANDOM_FOREST_MODEL')
+    model = joblib.load('../models/RANDOM_FOREST_MODEL')
     return model.predict(df_copy)
