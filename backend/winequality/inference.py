a5528ca5020137c604107da6f957ac94730a0204
Sergio Morales <s.mora.94@gmail.com>
2022-06-19 13:21:34 +0200

    Testing .env file

--- backend/winequality/inference.py
+++ backend/winequality/inference.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import joblib
+import os
 
 
 def make_predictions(data: pd.DataFrame) -> dict[str, str]:
     df_copy = data.copy()
-    model = joblib.load('models/RANDOM_FOREST_MODEL')
+    model = joblib.load(os.getenv('ROOT')+'/backend/models/RANDOM_FOREST_MODEL')
 
     return model.predict(df_copy)
