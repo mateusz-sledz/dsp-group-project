0c5c73054a3e2f8382f2c7b10b53f3348ba7156f
Sergio Morales <s.mora.94@gmail.com>
2022-06-21 04:32:24 +0200

    First trial of ML flow

--- backend/winequality/train.py
+++ backend/winequality/train.py
@@ -3,29 +3,36 @@ from dotenv import load_dotenv
 import joblib
 import pandas as pd
 import sklearn.metrics as metrics
+import mlflow
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 
 def build_model(data: pd.DataFrame) -> dict[str, float]:
     Y_train = data['quality']
-    X_train, X_test, y_train, y_test =\
+    X_train, X_test, y_train, y_test = \
         train_test_split(data.drop(['quality'], axis=1), Y_train, test_size=0.4,
-                                                        random_state=30)
+                         random_state=30)
+    with mlflow.start_run():
+        model = RandomForestClassifier()
+        model.fit(X_train, y_train)
 
-    model = RandomForestClassifier()
-    model.fit(X_train, y_train)
+        if os.getenv('ROOT') is None:
+            load_dotenv()
 
-    if os.getenv('ROOT') is None:
-        load_dotenv()
+        joblib.dump(model, os.getenv('ROOT') + '/backend/models/RANDOM_FOREST_MODEL')
 
-    joblib.dump(model, os.getenv('ROOT')+'/backend/models/RANDOM_FOREST_MODEL')
+        y_pred = model.predict(X_test)
 
-    y_pred = model.predict(X_test)
+        test_metrics = {
+            'accuracy': metrics.accuracy_score(y_test, y_pred),
+            'precision': metrics.precision_score(y_test, y_pred, average='micro'),
+            'recall': metrics.recall_score(y_test, y_pred, average='micro'),
+            'f-score': metrics.f1_score(y_test, y_pred, average='micro')
+        }
 
-    return {
-        'accuracy': metrics.accuracy_score(y_test, y_pred),
-        'precision': metrics.precision_score(y_test, y_pred, average='micro'),
-        'recall': metrics.recall_score(y_test, y_pred, average='micro'),
-        'f-score': metrics.f1_score(y_test, y_pred, average='micro')
-    }
+        print(model.get_params())
+        experiment_name = "wine_prediction_with_random_forest"
+        # mlflow.set_experiment(experiment_name)
+        # mlflow.log_metrics(test_metrics)
+    return test_metrics
