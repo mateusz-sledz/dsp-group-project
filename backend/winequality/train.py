217dde79290129e259ab5ff4efd39560891d8615
Sergio Morales <s.mora.94@gmail.com>
2022-06-22 03:02:20 +0200

    Added mlflow to train.py

--- backend/winequality/train.py
+++ backend/winequality/train.py
@@ -1,6 +1,7 @@
 import os
+import numpy as np
 from dotenv import load_dotenv
-import joblib
+# import joblib
 import pandas as pd
 import sklearn.metrics as metrics
 import mlflow
@@ -9,18 +10,30 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def build_model(data: pd.DataFrame) -> dict[str, float]:
-    Y_train = data['quality']
-    X_train, X_test, y_train, y_test =\
-        train_test_split(data.drop(['quality'], axis=1), Y_train, test_size=0.4)
+    if os.getenv('ROOT') is None:
+        load_dotenv()
 
-    with mlflow.start_run():
-        model = RandomForestClassifier()
+    mlflow_user = os.getenv('DB_USER')
+    mlflow_psw = os.getenv('DB_PSW')
+    artifact = os.getenv('ROOT') + '/mlruns'
+
+    tracking_uri = f"postgresql://{mlflow_user}:{mlflow_psw}@127.0.0.1:5432/winedb"
+    mlflow.set_tracking_uri(tracking_uri)
+    experiment_name = 'RandomForests_model'
+
+    if not mlflow.get_experiment_by_name(experiment_name):
+        mlflow.create_experiment(name=experiment_name, artifact_location=artifact)
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+
+    with mlflow.start_run(experiment_id=experiment.experiment_id, run_name=f"run_{experiment_name}"):
+        rand_state = np.random.randint(1, 100)
+
+        Y_train = data['quality']
+        X_train, X_test, y_train, y_test = \
+            train_test_split(data.drop(['quality'], axis=1), Y_train, test_size=0.4, random_state=rand_state)
+
+        model = RandomForestClassifier(random_state=rand_state)
         model.fit(X_train, y_train)
-
-        if os.getenv('ROOT') is None:
-            load_dotenv()
-
-        joblib.dump(model, os.getenv('ROOT') + '/backend/models/RANDOM_FOREST_MODEL')
 
         y_pred = model.predict(X_test)
 
@@ -31,9 +44,11 @@ def build_model(data: pd.DataFrame) -> dict[str, float]:
             'f-score': metrics.f1_score(y_test, y_pred, average='micro')
         }
 
+        params = model.get_params()
         print(model.get_params())
-        experiment_name = "wine_prediction_with_random_forest"
-        # mlflow.set_experiment(experiment_name)
-        # mlflow.log_metrics(test_metrics)
+
+        mlflow.sklearn.log_model(model, 'random forest classifier')
+        mlflow.log_params(params)
+        mlflow.log_metrics(test_metrics)
 
     return test_metrics
